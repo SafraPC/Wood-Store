482357630a099dbd8f610264bb3efe449ae158c9
Leandro Safra <59981353+SafraPC@users.noreply.github.com>
2021-02-04 07:40:49 -0300

    Can set data into Header

--- js/main.js
+++ js/main.js
@@ -1,2 +1,16 @@
 import {teste}from'./modules/header.js'
- teste();
+let val = teste;
+
+// const getHambuguer = document.querySelector(".dropDown");
+// function testeOi(){
+//     alert(val);
+// }
+// getHambuguer.addEventListener("click",testeOi);
+const iFrame = document.getElementById("iFrame");
+const hamburguer = iFrame.contentWindow.document.getElementById("hamburguer");
+console.log(hamburguer);
+function testeOi(){
+    console.log(val);
+}
+hamburguer.addEventListener("click",testeOi);
+// hamburguer.onClick = testeOi;
