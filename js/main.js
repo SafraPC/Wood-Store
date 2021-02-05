5ebee33cdd90226ff63a1067db7e68f37e101e75
Felipe Carradori <56209331+Carradori@users.noreply.github.com>
2021-02-05 18:01:54 -0300

    fix/styles: move the hamburguer logic to css

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
-import {teste}from './index.js'
+import "./test.js";
 const iFrame = document.getElementById("iFrame");
-const getIframe = document.getElementById("iFrame");
-const getHeader = iFrame.contentWindow.document.getElementById("getHeader");
+const header = iFrame.contentWindow.document.getElementById("nav-header");
 const hamburguer = iFrame.contentWindow.document.getElementById("hamburguer");
-    function TurnHeader(){
-        if(getHeader.style.display==="none"){
-            getHeader.style.display="flex";
-            getIframe.style.height="24rem";
-        }else{
-            getIframe.style.height="84px";
-            getHeader.style.display="none";
-        }
-    }
 
-    
-hamburguer.addEventListener("click",TurnHeader);
+function handleOpenCloseDropDown() {
+	header.classList.toggle("active");
+	iFrame.style.height = "24rem";
+	// if (getHeader.style.display === "none") {
+	// 	getHeader.style.display = "flex";
+	// 	iFrame.style.height = "24rem";
+	// } else {
+	// 	iFrame.style.height = "84px";
+	// 	getHeader.style.display = "none";
+	// }
+}
+hamburguer.addEventListener("click", handleOpenCloseDropDown);
 // hamburguer.onClick = testeOi;
