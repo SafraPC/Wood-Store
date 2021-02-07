798462b6de4c13d8495cebdf8c7e3c959f1775c4
Leandro Safra <59981353+SafraPC@users.noreply.github.com>
2021-02-07 14:25:36 -0300

    header is almost complete

--- components/header.js
+++ components/header.js
@@ -1,19 +1,18 @@
 const template = document.createElement("template");
 template.innerHTML=`
 <link rel="stylesheet" href="../css/headerStyles.css" />
-<link rel="stylesheet" href="./components/header-styles.css" />
 <div class ="nav">
     <div class="logo">
         <h4>Wood-Store</h4>
     </div>
     <div class="nav-item">
-        <a class="item"><div>Produtos</div></a>
-        <a class="item"><div>Quem Somos</div></a>
-        <a class="item"><div>Ajuda</div></a>
+        <a class="item" href="../html/produtos.html"><div>Produtos</div></a>
+        <a class="item" href="../index.html"><div>Quem Somos</div></a>
+        <a class="item" href="../html/ajuda.html"><div>Ajuda</div></a>
     </div>
     <div class="nav-lr">
-        <a class="item"><div >Login</div></a>
-        <a class="item"><div >Register</div></a>
+        <a class="item" href="/html/login.html"><div >Login</div></a>
+        <a class="item" href="/html/register.html"><div >Register</div></a>
     </div>
     <div class="burger"">
         <div></div>
@@ -41,6 +40,7 @@ template.innerHTML=`
             const burguer = this.shadowRoot.querySelector(".burger");
             const nav = this.shadowRoot.querySelector(".nav-item");
             const navTwo = this.shadowRoot.querySelector(".nav-lr");
+            
             this.handleClick(burguer, nav,navTwo);
         }
     }
