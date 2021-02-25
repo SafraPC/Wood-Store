fef0bef891e090295d0ddfc5e08cd0cba9acad98
Leandro Safra <59981353+SafraPC@users.noreply.github.com>
2021-02-25 19:21:20 -0300

    Need attencion

--- js/register.js
+++ js/register.js
@@ -1,6 +1,13 @@
 //JS File for register functions and masks
 let cpf = document.getElementById('cpf');
 let dataNasc = document.getElementById('dataNasc');
+
+
+//   cpf.onkeypress = (e) =>{
+//   console.log(e.key);
+// }
+
+//ARRUMAR AQUI DEPOIS
    cpf.addEventListener('input', (e)=> {
         var x = e.target.value.replace(/\D/g, '').match(/(\d{3})(\d{3})(\d{3})(\d{2})/);
         e.target.value = (x[1] + '.' + x[2] + '.' + x[3] + '-' + x[4]);
