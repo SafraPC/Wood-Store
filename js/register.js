1715bbf2347a4f90a75a667fd3fc6f52f8b119f1
Leandro Safra <59981353+SafraPC@users.noreply.github.com>
2021-02-25 15:10:38 -0300

    new regex added  / html cleaned

--- js/register.js
+++ js/register.js
@@ -1,6 +1,11 @@
 //JS File for register functions and masks
 let cpf = document.getElementById('cpf');
+let dataNasc = document.getElementById('dataNasc');
    cpf.addEventListener('input', (e)=> {
         var x = e.target.value.replace(/\D/g, '').match(/(\d{3})(\d{3})(\d{3})(\d{2})/);
         e.target.value = (x[1] + '.' + x[2] + '.' + x[3] + '-' + x[4]);
+      });
+      dataNasc.addEventListener('input', (e)=> {
+        var x = e.target.value.replace(/\D/g, '').match(/(\d{2})(\d{2})(\d{4})/);
+        e.target.value = (x[1] + '/' + x[2] + '/'+x[3] );
       });
