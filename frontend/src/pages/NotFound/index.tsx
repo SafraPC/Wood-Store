ce25c3f44297b3c3ba9e09b2c322bbfc56b53327
Leandro <leandrosafra.jconectada@gmail.com>
2022-01-20 15:43:40 -0300

    front: create not found page

---
+++ frontend/src/pages/NotFound/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Page } from "../../components/Page";
+
+const NotFound: React.FC = () => {
+	return (
+		<Page>
+			<h1>404</h1>
+			<h2>Page not Found!</h2>
+		</Page>
+	);
+};
+
+export default NotFound;
--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { Route, Routes } from "react-router";
+import { Navigate } from "react-router-dom";
 import Login from "./pages/Login";
+import NotFound from "./pages/NotFound";
 import WhoWeAre from "./pages/WhoWeAre";
 
 function AppRoutes() {
 	return (
 		<Routes>
+			<Route path="/" element={<Navigate to="/who-we-are" />} />
 			<Route path="/who-we-are" element={<WhoWeAre />} />
 			<Route path="/login" element={<Login />} />
+			<Route path="*" element={<NotFound />} />
 		</Routes>
 	);
 }
