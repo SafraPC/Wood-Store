f17326a490fa7fb8b9c430668322143bb93762df
Leandro <leandrosafra.jconectada@gmail.com>
2022-01-30 00:22:10 -0300

    backend: change status port

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,10 +4,12 @@ import routes from "./routes";
 import cors from "cors";
 const app = express();
 
+const port = 4000;
+
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(routes);
-app.listen(8080, () => {
-	console.log("Server is Running on Port 8080");
+app.listen(port, () => {
+	console.log(`Server is Running on Port ${port}`);
 });
